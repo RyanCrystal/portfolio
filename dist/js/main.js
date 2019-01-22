9a656f52226db4499eda655b4239b18d3c8a966c
Ryan <slaric2014@gmail.com>
2019-01-21 21:24:48 -0500

    fix contact not highlighted when scrolled

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,79 @@
+// Cache selectors
+var lastId,
+  topMenu = $(".navbar-items"),
+  topMenuHeight = topMenu.outerHeight() + 1,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) {
+      return item;
+    }
+  });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: offsetTop
+      },
+      950
+    );
+  e.preventDefault();
+});
+
+$(".arrow_down").click(function(e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: offsetTop
+      },
+      950
+    );
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function() {
+    if ($(this).offset().top < fromTop) return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent()
+      .removeClass("active")
+      .end()
+      .filter(`[href="#${id}"]`)
+      .parent()
+      .addClass("active");
+  }
+});
+
 const cate = document.querySelectorAll(".filter");
 const pro_items = document.querySelectorAll(".projects_items");
 const menu_btn = document.querySelector(".buttons");
 menu_btn.addEventListener("click", toggleActive);
 function toggleActive() {
   document.querySelector(".navbar-items").classList.toggle("active");
-}
-const menu_items = document.querySelectorAll(".nav-item");
-for (let i of menu_items) {
-  i.addEventListener("click", function() {
-    for (let k of menu_items) {
-      k.classList.remove("active");
-    }
-    i.classList.add("active");
-  });
 }
 
 // switch button tabs and get color
